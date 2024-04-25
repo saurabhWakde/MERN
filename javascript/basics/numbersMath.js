ce85385ab4d9d0eec9be4f960de1ab3e066daad1
saurabhWakde <128630330+saurabhWakde@users.noreply.github.com>
2024-04-26 03:24:53 +0530

    Math and numbers

---
+++ javascript/basics/numbersMath.js
@@ -0,0 +1,21 @@
+// const score = 53
+// const balance = new Number(100)
+// console.log(score, balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) // => 100.00
+// const otherNumber = 256.52
+// console.log(otherNumber.toPrecision(3))
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+// **************************Maths*************************
+
+// console.log(Math.abs(-100)) // => 100
+// console.log(Math.round(4.6)) // => 5
+// console.log(Math.ceil(4.6)) // => 5
+// console.log(Math.floor(4.6)) // => 4
+console.log((Math.random()*10)+1)
+const min = 1000;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
