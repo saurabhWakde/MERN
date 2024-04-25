a96f9a9304daa06f9e34fadd5e8cd477f3f64720
saurabhWakde <128630330+saurabhWakde@users.noreply.github.com>
2024-04-26 01:54:59 +0530

    data type summary

---
+++ javascript/basics/conversionOperations.js
@@ -0,0 +1,40 @@
+let score = "53abc" // suppose you dont know the what is datatype of this variable
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)  NaN Not a Number
+
+// "53" => 53
+// "53abc" => NaN
+// "true" => 1
+// "false" => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Saurabh" => true
+
+let number = 65*65
+
+let stringNumber = String(number)
+// console.log(typeof(stringNumber))
+// console.log(stringNumber)
+
+// *****************Operations*******************
+let value = 5
+let newValue = -value
+console.log(newValue)
+
+
+
+
+
---
+++ javascript/basics/datatypeSummary.js
@@ -0,0 +1,12 @@
+// Primitive 
+// 7 types : String, Number, BigInt, Boolean, Null, Undefined, Symbol
+
+// Non Primitive or refrence type
+// Object, Array, Function, Date, RegExp, Error, Map, Set, WeakMap, WeakSet, Promise, Proxy, TypedArray, etc.
+
+const id = Symbol("123")
+const value = Symbol("123")
+// console.log(id === value)
+let date = new Date
+// console.table({id,value,})
+console.log(typeof date)
